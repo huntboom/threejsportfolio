6c69ffb76afd45297605dda215223401c209e177
huntboom <hkarsseboom@hotmail.com>
2022-10-14 01:08:36 -0400

    Updates

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ renderer.render(scene, camera);
 // Torus
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: "#008080"});
+const material = new THREE.MeshStandardMaterial({ color: "purple"});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
@@ -79,7 +79,6 @@ const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
-    normalMap: normalTexture,
   })
 );
 
