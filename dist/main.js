24b7f6b57ccd538c0ec55d6d5a791b8091a1acee
huntboom <hkarsseboom@hotmail.com>
2024-08-25 12:00:13 -0400

    fixed sizing issues for mobile devices sections now dynamically render

--- dist/main.js
+++ dist/main.js
@@ -126,3 +126,12 @@ function animate() {
 }
 
 animate();
+
+// Handle window resize
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
